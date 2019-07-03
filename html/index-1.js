89f8bd4d70a60407f40762ef5690414fa7ef9e01
abdousalamb <52513588+abdousalamb@users.noreply.github.com>
2019-07-04 01:48:38 +0200

    Create index-1.js

---
+++ html/index-1.js
@@ -0,0 +1,4 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v3.3"></script>
+‎‎Step 2:‎ Place this code wherever you want the plugin to appear on your page.‎
+<div class="fb-like" data-href="https://www.facebook.com/Mercado-%D9%8B%D9%8B%D9%8E%D9%8E-309962839118134/" data-width="" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div
